22fb7a60b77a42e5c49df383e2cbafbfd10c2a2e
khalid karam <khalid.tasks@gmail.com>
2024-09-20 00:07:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MethodOverriding
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var dog = new ChildDog();
+            dog.Speak();
+        }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public virtual void Speak()
+        {
+            Console.WriteLine("Animal makes a sound.");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void Speak()
+        {
+            base.Speak();
+            Console.WriteLine("Dog makes a sound.");
+        }
+    }
+
+
+
+    public class ChildDog : Dog
+    {
+        public override void Speak()
+        {
+            base.Speak();
+            Console.WriteLine("Child Dog makes a sound.");
+        }
+    }
+
+
+}
